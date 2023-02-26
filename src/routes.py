67daeb7b1235be00834001ce040f529b23c34cd2
Spasennyi <c0mp2e5502@gmail.com>
2023-02-26 18:24:09 +0600

    extend func

--- src/routes.py
+++ src/routes.py
@@ -1,10 +1,13 @@
 from src import app
 from flask import render_template
+from .entities.books import menu
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           title="Главная страница",
+                           menu=menu)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='О сайте')
