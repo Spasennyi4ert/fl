ad1edea8664d848bd62f2f3c33da0793da12a479
Spasennyi <c0mp2e5502@gmail.com>
2023-03-01 17:45:02 +0600

    add login form

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,5 @@
 from src import app
-from flask import render_template, request
+from flask import render_template, request, session, redirect, url_for
 from .entities.books import menu
 
 @app.route("/")
@@ -18,3 +18,28 @@ def feedback():
     return render_template('feedback.html',
                            title='Отзывы',
                            menu=menu)
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('page404.html',
+                           title="Страница не найдена",
+                           menu=menu)
+
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Профиль пользователя: {username}"
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if "userLogged" in session:
+        return redirect(url_for('profile',
+                                username=session['userLogged']))
+    elif request.method == "POST" and request.form['username'] == "me" and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html',
+                           title="Авторизация",
+                           menu=menu)
+
+
